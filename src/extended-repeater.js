d52db84428ca0bdff22d85c9da65ebc49fd5e8b9
chasmy911 <chasmy@inbox.ru>
2022-10-16 16:27:40 +0300

    feat: count ready

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  str = String(str);
+  options.addition= String(options.addition);
+  
+  if (str.length == 0) {return str}
+  if (options.repeatTimes == 0) {return str }
+  if (!options["additionSeparator"]) options["additionSeparator"] = "|";
+  if (!options["separator"]) options["separator"] = "+";
+if (!options.addition) {
+  return str = (str+options.separator).repeat(options.repeatTimes-1) +str;}
+console.log(str);
+
+
+  if(options.addition) {
+
+   let addition1 = ((options.addition + options.additionSeparator).repeat(options.additionRepeatTimes - 1)) + options.addition
+  
+  let str1 = str + addition1;
+if (options.repeatTimes > 1 && options.additionRepeatTimes> 0) {
+  let str2 = (str1+options.separator).repeat(options.repeatTimes-1) +str1;
+  return str2
+}
+  }
+
 }
 
 module.exports = {
